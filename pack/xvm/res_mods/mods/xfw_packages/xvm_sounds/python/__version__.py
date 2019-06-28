e1bbd6bb2de82a6932b6b287755935c8e1865a56
B0bbyD0llar <andre@bitartist.com>
2019-06-28 15:12:18 +0200

    .919

--- pack/xvm/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.5'
+__xvm_version__ = '7.9.6'
 __wot_version__ = '1.5.1.1'
-__revision__ = '9179'
+__revision__ = '9196'
 __branch__ = 'default'
-__node__ = '4e10e02bf1fba50d807999bd32f98a4f536bebd3'
+__node__ = '2c43d50d5b9ac98ee92c7e99dd957c6d62feba25'
