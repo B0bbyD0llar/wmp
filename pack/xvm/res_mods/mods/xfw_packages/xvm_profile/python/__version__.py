2acfd33a84d75709601dffd750691eb7549d44f6
B0bbyD0llar <andre@bitartist.com>
2019-07-16 18:02:43 +0200

    .921

--- pack/xvm/res_mods/mods/xfw_packages/xvm_profile/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_profile/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.7'
-__wot_version__ = '1.5.1.1'
-__revision__ = '9209'
+__xvm_version__ = '7.9.8'
+__wot_version__ = '1.5.1.2'
+__revision__ = '9244'
 __branch__ = 'default'
-__node__ = '1367634c347a79d83d227db06584d4a6132297de'
+__node__ = 'aed96c8b2a322de278db5a9cc8d050962c758163'
