984f647a2b8d6e18e54cef198a4f6a8e3b9601c8
B0bbyD0llar <andre@bitartist.com>
2019-08-31 16:02:08 +0200

    .928

--- pack/xvm/res_mods/mods/xfw_packages/xvm_contacts/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_contacts/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.2'
-__wot_version__ = '1.6.0.3'
-__revision__ = '9312'
+__xvm_version__ = '8.0.3'
+__wot_version__ = '1.6.0.4'
+__revision__ = '9317'
 __branch__ = 'default'
-__node__ = 'da27d30ed71547b2235e23238a9740166790658d'
+__node__ = 'ad2b3647e0c325fe80d19c4733e6f52e039feca2'
