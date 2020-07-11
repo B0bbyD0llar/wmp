131519772e0f11cc4c4e60dcfd31a77e9c370ea5
B0bbyD0llar <andre@bitartist.com>
2020-07-11 13:05:13 +0200

    .954

--- pack/xvm/res_mods/mods/xfw_packages/xvm_battleresults/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_battleresults/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.5.4'
-__wot_version__ = '1.9.1.1'
-__revision__ = '0055'
+__xvm_version__ = '8.5.7'
+__wot_version__ = '1.9.1.2'
+__revision__ = '0005'
 __branch__ = 'master'
-__node__ = 'a3fc4a62c66a06665e062105649f6ff49509cecd'
+__node__ = '76778d9a53b6d9c541381602687aa9d57a6e1552'
 __development__ = 'False'
