51236b581c78df5a8c4f52ab4d164a266d0d449f
B0bbyD0llar <andre@bitartist.com>
2020-12-25 11:53:21 +0100

    .965

--- pack/xvm/res_mods/mods/xfw_packages/xvm_squad/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_squad/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.7.0'
+__xvm_version__ = '8.7.1'
 __wot_version__ = '1.11.0.0'
-__revision__ = '0018'
+__revision__ = '0006'
 __branch__ = 'master'
-__node__ = 'b224851f07129777d17c07db93890d312e1d2f0e'
+__node__ = 'bc8dac8ca43887f429fd7dda23e203c987479e4f'
 __development__ = 'False'
