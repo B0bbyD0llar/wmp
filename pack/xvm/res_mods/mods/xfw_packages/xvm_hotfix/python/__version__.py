9aa8367b61f5d9f4ecd6cb0ef5ab3f90dcbed653
B0bbyD0llar <andre@bitartist.com>
2021-02-03 19:12:12 +0100

    .968

--- pack/xvm/res_mods/mods/xfw_packages/xvm_hotfix/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_hotfix/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.7.3'
-__wot_version__ = '1.11.1.1'
-__revision__ = '0008'
+__xvm_version__ = '8.7.5'
+__wot_version__ = '1.11.1.2'
+__revision__ = '0012'
 __branch__ = 'master'
-__node__ = '77c1edddde8f2d00bfd128c295de2253671b5712'
+__node__ = '3a908bfad800ab76d3213ae18e2c0f7d78c58908'
 __development__ = 'False'
