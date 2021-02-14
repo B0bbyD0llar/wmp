16cce19cb03ac231f72c8f25fca260f5002d9740
B0bbyD0llar <andre@bitartist.com>
2021-02-14 11:22:35 +0100

    .970

--- pack/xvm/res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
+++ pack/xvm/res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
 __xvm_version__ = '8.7.6'
 __wot_version__ = '1.11.1.3'
-__revision__ = '0004'
+__revision__ = '0005'
 __branch__ = 'master'
-__node__ = '97187d7c32a3deafb124a15eac025a8d26391fd1'
-__development__ = 'True'
+__node__ = '8a17ddc87f98005c539691ff3df5a82e75beb0e5'
+__development__ = 'False'
